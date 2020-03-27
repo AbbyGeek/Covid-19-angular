e4ddd8b0d933f3a6caf7e6df1e294d3ffadd43c0
Taylor Everts <tayloreverts@quickenloans.com>
2020-03-27 18:42:05 -0400

    built europe component

--- src/app/general-info/general-info.component.ts
+++ src/app/general-info/general-info.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-general-info',
@@ -8,28 +6,11 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./general-info.component.css']
 })
 export class GeneralInfoComponent implements OnInit {
-  restItems: any;
-  restItemsUrl = 'https://corona.lmao.ninja/countries';
+ @Input() generalInfoResult: any;
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
   ngOnInit() {
-    this.getRestItems();
   }
-  //read all REST items
-  getRestItems(): void {
-    this.restItemsServiceGetRestItems()
-      .subscribe(
-        restItems => {
-          this.restItems = restItems;
-          console.log(this.restItems)
-        }
-      )
-  }
-  //Rest Items Service: Read all REST Items
-  restItemsServiceGetRestItems() {
-    return this.http 
-      .get<any[]>(this.restItemsUrl)
-      .pipe(map(data=>data))
-  }
+ 
 }
--- src/app/usa/usa.component.ts
+++ src/app/usa/usa.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-usa',
@@ -8,27 +6,11 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./usa.component.css']
 })
 export class USAComponent implements OnInit {
-  restItems: any;
-  restItemsURL = "https://corona.lmao.ninja/states";
+  @Input() usaResult: any;
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
   ngOnInit() {
-    this.getRestItems();
   }
   //read all REST items
-  getRestItems(): void {
-    this.restItemsServiceGetRestItems()
-      .subscribe(
-        restItems => {
-          this.restItems = restItems;
-        }
-      )
-  }
-  //Rest Items Service: Read all REST Items
-  restItemsServiceGetRestItems() {
-    return this.http 
-      .get<any[]>(this.restItemsURL)
-      .pipe(map(data=>data))
-  }
 }
